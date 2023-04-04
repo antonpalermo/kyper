21ec2cf617625736ee9e355efe10bdbd01cb2167
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2023-04-04 22:25:44 +0800

    feat: create tasks (#2)
    * ability to create task.
    
    * display all created tasks.
    
    * removed package-locks
    
    * adjust tasks cache

---
+++ src/components/Tasks.tsx
@@ -0,0 +1,18 @@
+import { api } from "@kyper/utils/api"
+
+export default function Tasks() {
+  const { data: tasks } = api.task.getAll.useQuery(undefined, {
+    staleTime: 1 * 60 * 1000
+  })
+
+  return (
+    <div>
+      {tasks &&
+        tasks.map(task => (
+          <div key={task.id}>
+            <h2>{task.subject}</h2>
+          </div>
+        ))}
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,51 @@
+import Tasks from "@kyper/components/Tasks"
+import { api } from "@kyper/utils/api"
+import { Form, Formik, type FormikHelpers } from "formik"
+
+type TaskInput = {
+  subject: string
+}
+
 export default function Home() {
-  return <h1>Home App</h1>
+  const ctx = api.useContext()
+
+  const { mutate, isLoading: isCreating } = api.task.create.useMutation({
+    onSuccess: async () => {
+      // TODO: add react hot toast.
+      await ctx.task.getAll.invalidate()
+    }
+  })
+
+  const initialData: TaskInput = {
+    subject: ""
+  }
+
+  function onSubmit({ subject }: TaskInput, _: FormikHelpers<TaskInput>) {
+    mutate({ subject })
+  }
+
+  return (
+    <div>
+      <Formik initialValues={initialData} onSubmit={onSubmit}>
+        {({ values, handleChange, isSubmitting }) => (
+          <Form>
+            <h1>Create Task</h1>
+            <div>
+              <label htmlFor="subject">Subject</label>
+              <input
+                id="subject"
+                name="subject"
+                value={values.subject}
+                onChange={handleChange}
+              />
+            </div>
+            <button disabled={isSubmitting && isCreating} type="submit">
+              Create
+            </button>
+          </Form>
+        )}
+      </Formik>
+      <Tasks />
+    </div>
+  )
 }
